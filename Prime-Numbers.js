795671d488b790b9c0f235c7d316fcfc220c372d
Dimitris Karakaxas <karakaxasdimitrios@yahoo.com>
2021-05-09 11:35:34 +0300

    Improved => O(sqrt(n))

--- Prime-Numbers.js
+++ Prime-Numbers.js
@@ -1,11 +1,13 @@
 function isPrime(num) {
-  for (let i = 2; i <= num / 2; i++) {
-    console.log("running");
-    if (num % i === 0) {
-      return false;
-    }
+  if (num % 1 !== 0 || num <= 1) return false;
+
+  if (num <= 3) return true;
+
+  if (num % 2 === 0) return false;
+
+  const endDivider = Math.sqrt(num);
+  for (let divider = 3; divider <= endDivider; divider++) {
+    if (num % divider === 0) return false;
   }
   return true;
 }
-
-console.log(isPrime(34559));

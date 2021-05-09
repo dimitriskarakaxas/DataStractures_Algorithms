33d9f307a06efbb235cce421b3f05387ba1af600
Dimitris Karakaxas <karakaxasdimitrios@yahoo.com>
2021-05-09 20:11:59 +0300

    ADD: new algorithms

---
+++ Is-Power-Of-Two.js
@@ -0,0 +1,18 @@
+function isPowerofTwo(number) {
+  if (number < 1) return false;
+  let dividedNumber = number;
+  while (dividedNumber !== 1) {
+    if (dividedNumber % 2 !== 0) return false;
+    dividedNumber = dividedNumber / 2;
+  }
+  return true;
+}
+// Best Case: number = 13 => O(1)
+// Average Case: O(log n)
+// Worst Case: 1,125,899,906,842,624 => O(log n)
+
+function isPowerOfTwoBinary(number) {
+  if (number < 1 || number % 2 !== 0) return false;
+  return (number & (number - 1)) === 0;
+}
+// Time Complexity => O(1)
